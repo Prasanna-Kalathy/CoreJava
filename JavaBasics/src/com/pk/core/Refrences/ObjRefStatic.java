3ee327a98602d45288df37d0b1d25363f4fe61ce
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-11-17 22:13:12 +0530

    Declared and displayed Object Refrence inside Static block

---
+++ JavaBasics/src/com/pk/core/Refrences/ObjRefStatic.java
@@ -0,0 +1,12 @@
+package com.pk.core.Refrences;
+
+public class ObjRefStatic {
+	static {
+		ObjRefStatic obj = new ObjRefStatic();
+		System.out.println(obj);
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
