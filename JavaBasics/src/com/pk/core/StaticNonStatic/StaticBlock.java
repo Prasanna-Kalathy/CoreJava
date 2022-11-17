116b950f816330252c8c4b76ca0f67e3c6d0421e
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-11-17 20:55:53 +0530

    Static Blocks

---
+++ JavaBasics/src/com/pk/core/StaticNonStatic/StaticBlock.java
@@ -0,0 +1,13 @@
+package com.pk.core.StaticNonStatic;
+
+public class StaticBlock {
+	public static void main(String[] args) {
+		System.out.println("Inside Main");
+	}
+	static {
+		System.out.println("Inside Static 1");
+	}
+	static {
+		System.out.println("Inside Static 2");
+	}
+}
