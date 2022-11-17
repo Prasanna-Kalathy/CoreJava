7ed3b5471a9f7b285b79f8b2150089ff1eb8ea33
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-11-17 22:19:09 +0530

    Declared outside and displayed Object Refrence inside Static block and main method

--- JavaBasics/src/com/pk/core/Refrences/ObjRefStatic.java
+++ JavaBasics/src/com/pk/core/Refrences/ObjRefStatic.java
@@ -1,12 +1,16 @@
 package com.pk.core.Refrences;
 
 public class ObjRefStatic {
+	static ObjRefStatic obj;
 	static {
-		ObjRefStatic obj = new ObjRefStatic();
-		System.out.println(obj);
+		//obj is declared but not initialised
+		System.out.println("Before Object Creation "+ObjRefStatic.obj);
+		ObjRefStatic.obj = new ObjRefStatic();
+		System.out.println("After Object Creation "+ObjRefStatic.obj);
 	}
 	
 	public static void main(String[] args) {
+		System.out.println("After Object Creation inside Main "+ObjRefStatic.obj);
 		
 	}
 }
