a12ce89f0298f6d982ee100e5f7363721fbc22f4
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-11-17 22:09:28 +0530

    Object Refrence inside main

---
+++ JavaBasics/src/com/pk/core/Refrences/ObjectRefrence.java
@@ -0,0 +1,10 @@
+package com.pk.core.Refrences;
+
+public class ObjectRefrence {
+	int x;
+
+	public static void main(String[] args) {
+		ObjectRefrence obj = new ObjectRefrence();
+		System.out.println(obj.x);
+	}
+}
