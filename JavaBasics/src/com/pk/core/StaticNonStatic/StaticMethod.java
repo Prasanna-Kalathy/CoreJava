183037fccce7c5226d889f8c646c1bee922473da
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-11-17 21:01:21 +0530

    Static Methods

---
+++ JavaBasics/src/com/pk/core/StaticNonStatic/StaticMethod.java
@@ -0,0 +1,23 @@
+package com.pk.core.StaticNonStatic;
+
+public class StaticMethod {
+	public static void main(String[] args) {
+		System.out.println("Inside Main");
+		StaticMethod.method2();
+		StaticMethod.method1();
+	}
+
+	static void method1() {
+		System.out.println("Inside Method 1");
+	}
+
+	static void method2() {
+		System.out.println("Inside Method 2");
+	}
+
+	static {
+		StaticMethod.method2();
+		StaticMethod.method1();
+
+	}
+}
