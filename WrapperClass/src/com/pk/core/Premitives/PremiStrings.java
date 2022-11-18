6850ee6fd165dcb98bcc668a2ec9674545156f29
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-11-18 21:08:31 +0530

    Premitive <-> Strings

---
+++ WrapperClass/src/com/pk/core/Premitives/PremiStrings.java
@@ -0,0 +1,12 @@
+package com.pk.core.Premitives;
+
+public class PremiStrings {
+	public static void main(String[] args) {
+		long o = 1365468465435l;
+		System.out.println(o);
+		String s = Long.toString(o);
+		System.out.println(s);
+		long z = Long.parseLong(s);
+		System.out.println(z);
+	}
+}
