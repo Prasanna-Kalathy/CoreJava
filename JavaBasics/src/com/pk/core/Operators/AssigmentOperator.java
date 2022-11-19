5510dff75e5fbac5b46fe17b1e404f564686a9a1
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-11-19 20:48:48 +0530

    Assignment Operator

---
+++ JavaBasics/src/com/pk/core/Operators/AssigmentOperator.java
@@ -0,0 +1,19 @@
+package com.pk.core.Operators;
+
+public class AssigmentOperator {
+	public static void main(String[] args) {
+		int a =45;
+		int x,u,i;
+		
+		x=u=i=45;
+		
+		System.out.println(x);
+		System.out.println(u);
+		System.out.println(i);
+		
+		int n=108;
+		
+		n-=98;	//n=n-98
+		System.out.println(n);
+	}
+}
---
+++ JavaBasics/src/com/pk/core/Operators/BitwiseOperator.java
@@ -0,0 +1,13 @@
+package com.pk.core.Operators;
+
+public class BitwiseOperator {
+	public static void main(String[] args) {
+		// Bools
+		System.out.println(true & true);
+		System.out.println(false | false);
+		System.out.println(true ^ false);
+		System.out.println(3 ^ 5);
+		System.out.println(3 & 5);
+		System.out.println(3 | 5);
+	}
+}
