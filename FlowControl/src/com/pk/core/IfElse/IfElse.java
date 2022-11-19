4dbaa39bb7ac1ab83d649252bcbc81ade429af64
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-11-19 22:21:52 +0530

    If else and Switch

---
+++ FlowControl/src/com/pk/core/IfElse/IfElse.java
@@ -0,0 +1,16 @@
+package com.pk.core.IfElse;
+
+public class IfElse {
+	public static void main(String[] args) {
+		int a=7,b=8;
+		
+		if (a>b) {
+			System.out.println("a is greater");
+		}else if(a==b){
+			System.out.println("Both are same");
+		}else {
+			System.out.println("b is greater");
+			
+		}
+	}
+}
---
+++ FlowControl/src/com/pk/core/Switch/Switch.java
@@ -0,0 +1,21 @@
+package com.pk.core.Switch;
+
+public class Switch {
+	public static void main(String[] args) {
+		String s = "98";
+		switch (s) {
+		case ("95"): {
+			System.out.println("95");
+			break;
+		}
+		case ("98"): {
+			System.out.println("98");
+			break;
+		}
+		default: {
+			System.out.println("None");
+		}
+
+		}
+	}
+}
