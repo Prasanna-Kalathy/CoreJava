052442f1dff60334c8d3ec7c93150a9bfd3c8da0
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-11-19 10:23:28 +0530

    Relational Opertaors

---
+++ JavaBasics/src/com/pk/core/Operators/RelationalOperator.java
@@ -0,0 +1,12 @@
+package com.pk.core.Operators;
+
+public class RelationalOperator {
+
+	public static void main(String[] args) {
+		System.out.println(10<=15);
+		System.out.println((int)'a'+" "+(int)'b');
+		System.out.println('a'>15);
+		System.out.println('a'== 'A');
+	}
+
+}
