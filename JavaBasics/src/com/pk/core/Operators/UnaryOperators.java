ee8785a4892d1e020697ae00c5afcc12754df7f7
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-11-19 09:40:03 +0530

    Unary - PostIncrement

--- JavaBasics/src/com/pk/core/Operators/UnaryOperators.java
+++ JavaBasics/src/com/pk/core/Operators/UnaryOperators.java
@@ -4,7 +4,7 @@ public class UnaryOperators {
 
 	public static void main(String[] args) {
 		int a = 10;
-		int y=++a;
+		int y=a++;
 		
 		System.out.println("value of a is "+a);
 		System.out.println("value of y is "+y);
