c8cae9fdc19863e110a46ddd20c07e62c7194860
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-11-19 22:25:01 +0530

    Switch Fall through

---
+++ FlowControl/src/com/pk/core/Switch/SwitchFall.java
@@ -0,0 +1,18 @@
+package com.pk.core.Switch;
+
+public class SwitchFall {
+	public static void main(String[] args) {
+		int s = 2;
+		switch (s) {
+		case (1):
+		case (2): {
+			System.out.println("Common Logic");
+			break;
+		}
+		default: {
+			System.out.println("None");
+		}
+
+		}
+	}
+}
