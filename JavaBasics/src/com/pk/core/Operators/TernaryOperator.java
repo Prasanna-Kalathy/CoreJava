440ea3b7305cf3ab1ed46b0a32d270014eef929a
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-11-19 20:54:59 +0530

    Ternary Operator

---
+++ JavaBasics/src/com/pk/core/Operators/TernaryOperator.java
@@ -0,0 +1,11 @@
+package com.pk.core.Operators;
+
+public class TernaryOperator {
+	public static void main(String[] args) {
+		int x=90,y=60;
+		
+		String result = (x>y) ? "x is greater" : "y is greater";
+		
+		System.out.println(result);
+	}
+}
