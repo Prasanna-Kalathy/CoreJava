18e58ff468d1624301113b66dc6c7a137b2dacb9
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-11-19 09:48:36 +0530

    Arithmetic Operators

---
+++ JavaBasics/src/com/pk/core/Operators/ArithmeticOperators.java
@@ -0,0 +1,14 @@
+package com.pk.core.Operators;
+
+public class ArithmeticOperators {
+	public static void main(String[] args) {
+		int a=10;
+		int b=5;
+		System.out.println("Add "+(a+b));
+		System.out.println("Sub "+(a-b));
+		System.out.println("Mul "+(a*b));
+		System.out.println("div "+(a/b));
+		System.out.println("mod "+(a%b));
+	
+	}
+}
