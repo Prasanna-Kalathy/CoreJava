9eb90c6b142e36d63572e005b262c8bbf07b57ff
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-11-20 22:49:06 +0530

    Updates codes

--- FlowControl/src/com/pk/core/Loops/DoWhileLoop.java
+++ FlowControl/src/com/pk/core/Loops/DoWhileLoop.java
@@ -2,13 +2,12 @@ package com.pk.core.Loops;
 
 public class DoWhileLoop {
 	public static void main(String[] args) {
-		int i = 1;
+
 		do {
-			System.out.println("Inside Do while");
-		} while (i < 2);
-		{
-			System.out.println(i);
-			i++;
+			System.out.println("Inside Do");
+		} while (false);{
+			System.out.println("Inside While");
 		}
+
 	}
 }
