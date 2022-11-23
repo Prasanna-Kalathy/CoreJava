b83a535a0a93c8ea679a86e1aed45966447a7ebe
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-11-23 22:45:58 +0530

    Static Imports

--- Packages&AccessMods/src/com/pk/core/BuiltInClass/ScannerUIP.java
+++ Packages&AccessMods/src/com/pk/core/BuiltInClass/ScannerUIP.java
@@ -1,17 +1,19 @@
 package com.pk.core.BuiltInClass;
 
 import java.util.Scanner;
+import static java.lang.System.out;
+import static java.lang.Integer.*;
 
 public class ScannerUIP {
 
 	public static void main(String[] args) {
 		Scanner In = new Scanner(System.in);
-		System.out.println("Enter 1st Number");
+		out.println("Enter 1st Number");
 		int Num1 = In.nextInt();
-		System.out.println("Enter 2nd Number");
+		out.println("Enter 2nd Number");
 		int Num2 = In.nextInt();
 
-		System.out.println(Num1 + Num2);
+		out.println(Num1 + Num2);
 	}
 
 }
