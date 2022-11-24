b5ad0d91663705d4034f2c427fe4378d27ecfa3d
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-11-24 16:58:08 +0530

    static import Assignment

---
+++ Asssignments/src/com/pk/core/assignments/packages/StaticImportAssignment.java
@@ -0,0 +1,13 @@
+package com.pk.core.assignments.packages;
+
+import static java.lang.Math.*;
+
+public class StaticImportAssignment {
+
+	public static void main(String[] args) {
+		int a = 15;
+		double d = sqrt(a);
+		System.out.println(d);
+	}
+
+}
