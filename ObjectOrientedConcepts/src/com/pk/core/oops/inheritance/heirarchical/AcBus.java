038c65f8a60e67b3c8183bc8eab1fcc3ebf7efc1
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-11-25 21:33:30 +0530

    **Unclear Super keyword and method**

--- ObjectOrientedConcepts/src/com/pk/core/oops/inheritance/heirarchical/AcBus.java
+++ ObjectOrientedConcepts/src/com/pk/core/oops/inheritance/heirarchical/AcBus.java
@@ -1,14 +1,11 @@
 package com.pk.core.oops.inheritance.heirarchical;
 
-public class Bus extends Vehicle {
-	@Override
-	String Fuel() {
-		super.Fuel();
-		return "CNG";
-	}
-	
+public class AcBus{
+
 	public static void main(String[] args) {
 		Bus bus = new Bus();
 		System.out.println(bus.Fuel());
+		
 	}
+
 }
