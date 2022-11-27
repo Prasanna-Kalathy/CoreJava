10f132a780915c81c12989c9b1c26fd2f9a3df08
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-11-27 22:30:28 +0530

    Abstarct class and main method inside it

--- ObjectOrientedConcepts/src/com/pk/core/oops/abstraction/BMW.java
+++ ObjectOrientedConcepts/src/com/pk/core/oops/abstraction/BMW.java
@@ -1,15 +1,13 @@
 package com.pk.core.oops.abstraction;
 
-public class AbstrationDemo {
-	
-	void commonFunc(){
+public abstract class BMW {
+
+	void commonFunc() {
 		System.out.println("Inside commonFunc method");
 	}
-	
+
 	abstract void accelerate();
-	
-	abstract void brake();
-	
+
 	public static void main(String[] args) {
 		System.out.println("Inside the main method");
 	}
