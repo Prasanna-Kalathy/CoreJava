a61f4a160185147e4a6b0238e95779fe7d5be5ea
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-11-30 22:06:37 +0530

    Interfaces in Oops

---
+++ ObjectOrientedConcepts/src/com/pk/core/oops/interfaces/Car.java
@@ -0,0 +1,8 @@
+package com.pk.core.oops.interfaces;
+
+public interface Car {
+	
+	void go();
+	void stop();
+
+}
--- ObjectOrientedConcepts/src/com/pk/core/oops/polymorphism/PolimorphismDemo.java
+++ ObjectOrientedConcepts/src/com/pk/core/oops/polymorphism/PolimorphismDemo.java
@@ -2,6 +2,6 @@ package com.pk.core.oops.polymorphism;
 
 public class PolimorphismDemo {
 	public static void main(String[] args) {
-		
+
 	}
 }
