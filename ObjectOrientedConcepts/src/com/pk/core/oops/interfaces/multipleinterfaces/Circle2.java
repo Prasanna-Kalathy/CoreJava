370dcd2de629a9851e0037e5b597ffee56486508
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-12-04 20:35:21 +0530

    more on interfaces

--- ObjectOrientedConcepts/src/com/pk/core/oops/interfaces/multipleinterfaces/Circle2.java
+++ ObjectOrientedConcepts/src/com/pk/core/oops/interfaces/multipleinterfaces/Circle2.java
@@ -2,5 +2,5 @@ package com.pk.core.oops.interfaces.multipleinterfaces;
 
 public interface Circle2 {
 	float pi=3.14f;
-	void calculateArea(float radius);
+	void calculateArea();
 }
--- ObjectOrientedConcepts/src/com/pk/core/oops/interfaces/multipleinterfaces/MyCircle.java
+++ ObjectOrientedConcepts/src/com/pk/core/oops/interfaces/multipleinterfaces/MyCircle.java
@@ -1,19 +1,14 @@
 package com.pk.core.oops.interfaces.multipleinterfaces;
 
-public class MyCircle implements Circle,Circle2{
+public class MyCircle implements Circle, Circle2 {
 
 	public static void main(String[] args) {
-		
+
 	}
-	
+
 	@Override
 	public void calculateArea() {
 		System.out.println(Circle2.pi);
-	}
-
-	@Override
-	public void calculateArea(float radius) {
-		
 	}
 
 }
