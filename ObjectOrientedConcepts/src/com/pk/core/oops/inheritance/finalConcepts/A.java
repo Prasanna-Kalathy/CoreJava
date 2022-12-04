018719e39b9739dd0e1218c06d91cead5b4d26fa
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-12-04 18:31:47 +0530

    Final Keyword with static usage -> Constant

--- ObjectOrientedConcepts/src/com/pk/core/oops/inheritance/finalConcepts/A.java
+++ ObjectOrientedConcepts/src/com/pk/core/oops/inheritance/finalConcepts/A.java
@@ -1,11 +1,7 @@
 package com.pk.core.oops.inheritance.finalConcepts;
 
 public final class A {
-	final float a;
-	
-	A(){
-		a=5.658f;
-	}
+	final static float a=3.14f;
 	
 	public static void main(String[] args) {
 		A obj = new A();
