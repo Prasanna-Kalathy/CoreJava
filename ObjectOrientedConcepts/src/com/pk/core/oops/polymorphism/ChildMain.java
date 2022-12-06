1cfd4158bd008031398e07ac12f00cf2a4436430
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-12-06 21:00:26 +0530

    Overriding main method

---
+++ ObjectOrientedConcepts/src/com/pk/core/oops/polymorphism/ChildMain.java
@@ -0,0 +1,7 @@
+package com.pk.core.oops.polymorphism;
+
+public class ChildMain extends ParentMain{
+	public static void main(String[] args) {
+		System.out.println("Main Method in Child Class");
+	}
+}
--- ObjectOrientedConcepts/src/com/pk/core/oops/polymorphism/ParentMain.java
+++ ObjectOrientedConcepts/src/com/pk/core/oops/polymorphism/ParentMain.java
@@ -1,5 +1,7 @@
 package com.pk.core.oops.polymorphism;
 
 public class ParentMain {
-
+	public static void main(String[] args) {
+		System.out.println("Main Method in Parent Class");
+	}
 }
