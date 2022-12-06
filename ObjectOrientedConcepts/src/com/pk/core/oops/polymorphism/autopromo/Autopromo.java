fe6a8e5d18a383f3aff26c6d22195c6c693ee255
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-12-06 20:06:10 +0530

    Auto Promotion in Overloading with object type

--- ObjectOrientedConcepts/src/com/pk/core/oops/polymorphism/autopromo/Autopromo.java
+++ ObjectOrientedConcepts/src/com/pk/core/oops/polymorphism/autopromo/Autopromo.java
@@ -6,11 +6,20 @@ public class Autopromo {
 	}
 
 	public void print(double f) {
-		System.out.println("Double version version " + f);
+		System.out.println("Double version " + f);
 	}
 
+//	public void print(String s) {
+//		System.out.println("String version " + s);
+//	}
+
+	public void print(Object o) {
+		System.out.println("Object version " + o);
+	}
+	
 	public static void main(String[] args) {
 		Autopromo ap = new Autopromo();
+		ap.print("Uzumaki");
 		ap.print(535.641f);
 		ap.print(54);
 	}
