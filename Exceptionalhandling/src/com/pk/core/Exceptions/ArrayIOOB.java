aee56e0b2ab14b1bfa323816dbf1398a73ddbdd9
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-12-08 13:57:28 +0530

    Array Index out of bound Exception Handled

--- Exceptionalhandling/src/com/pk/core/Exceptions/ArrayIOOB.java
+++ Exceptionalhandling/src/com/pk/core/Exceptions/ArrayIOOB.java
@@ -3,12 +3,19 @@ package com.pk.core.Exceptions;
 public class ArrayIOOB {
 	public static void main(String[] args) {
 		int ary[] = { 45, 98, 65 };
-		System.out.println("Elementts in the array are ");
-		for (int i = 0; i <= ary.length; i++) {
-			System.out.println(ary[i]);
+
+		try {
+			System.out.println("Elementts in the array are ");
+			for (int i = 0; i <= ary.length; i++) {
+				System.out.println(ary[i]);
+			}
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Index Beyond Array Length");
 		}
+
 		System.out.println("Code after Array output");
 	}
+
 	public void display() {
 		System.out.println("In ArryIOOB class");
 	}
