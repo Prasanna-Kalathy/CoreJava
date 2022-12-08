001c7c8392cf98792593907e96184034d4089979
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-12-08 13:59:32 +0530

    String Parser Exception handled

--- Exceptionalhandling/src/com/pk/core/Exceptions/StringParser.java
+++ Exceptionalhandling/src/com/pk/core/Exceptions/StringParser.java
@@ -3,9 +3,13 @@ package com.pk.core.Exceptions;
 public class StringParser {
 
 	public static void main(String[] args) {
-		String s ="absd";
-		
-		int i = Integer.parseInt(s);
+		String s = "absd";
+		try {
+			int i = Integer.parseInt(s);
+			System.out.println(i);
+		} catch (Exception e) {
+			System.out.println("Enter Proper Number for Parsing into integer ");
+		}
 	}
 
 }
