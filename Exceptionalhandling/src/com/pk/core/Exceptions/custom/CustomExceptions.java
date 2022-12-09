343bfb91cc1b502cdf0b37253fe57ab717eac145
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-12-09 19:56:52 +0530

    Throw keyword

---
+++ Exceptionalhandling/src/com/pk/core/Exceptions/custom/CustomExceptions.java
@@ -0,0 +1,9 @@
+package com.pk.core.Exceptions.custom;
+
+public class CustomExceptions {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ Exceptionalhandling/src/com/pk/core/Exceptions/custom/Throwdemo.java
@@ -0,0 +1,7 @@
+package com.pk.core.Exceptions.custom;
+
+public class Throwdemo {
+	public static void main(String[] args) {
+		throw new RuntimeException("Running out of funds");
+	}
+}
