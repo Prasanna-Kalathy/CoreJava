1ecfd866840c8d3ecbc3e47299f245b969cdc9c9
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-12-09 21:16:09 +0530

    Assertion with 1 expression

---
+++ Exceptionalhandling/src/com/pk/core/Exceptions/Assertions/Assertion.java
@@ -0,0 +1,11 @@
+package com.pk.core.Exceptions.Assertions;
+
+public class Assertion {
+
+	public static void main(String[] args) {
+		int WithDrawn = 200;
+		int Bal = 100;
+		
+		assert (WithDrawn <= Bal);
+	}
+}
