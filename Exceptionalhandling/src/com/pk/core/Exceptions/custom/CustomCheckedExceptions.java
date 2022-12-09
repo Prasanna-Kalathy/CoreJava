2aa1971532bef19cc95f91f07aacd8e8da499841
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-12-09 20:37:24 +0530

    Custom checked Exception

---
+++ Exceptionalhandling/src/com/pk/core/Exceptions/custom/CustomCheckedExceptions.java
@@ -0,0 +1,13 @@
+package com.pk.core.Exceptions.custom;
+
+public class CustomCheckedExceptions extends Exception {
+
+	CustomCheckedExceptions(String message) {
+		super(message);
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
--- Exceptionalhandling/src/com/pk/core/Exceptions/custom/CustomExpTest.java
+++ Exceptionalhandling/src/com/pk/core/Exceptions/custom/CustomExpTest.java
@@ -1,7 +1,8 @@
 package com.pk.core.Exceptions.custom;
 
 public class CustomExpTest {
-	public static void main(String[] args) {
-		throw new CustomUncheckedExceptions("Maintenance Work is Going on");
+	public static void main(String[] args) throws CustomCheckedExceptions {
+//		throw new CustomUncheckedExceptions("Maintenance Work is Going on");
+		throw new CustomCheckedExceptions("Some one turned off the power");
 	}
 }
