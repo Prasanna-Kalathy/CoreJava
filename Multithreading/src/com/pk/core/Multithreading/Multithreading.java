c3996e978e2aa1a74bd5708b19cfeae088a4bab7
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-12-10 23:09:51 +0530

    Multi Threaded demo with sleep

--- Multithreading/src/com/pk/core/Multithreading/Multithreading.java
+++ Multithreading/src/com/pk/core/Multithreading/Multithreading.java
@@ -2,21 +2,26 @@ package com.pk.core.Multithreading;
 
 public class Multithreading extends Thread {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		Multithreading mt = new Multithreading();
 		mt.start();
 		System.out.println("between loops");
 		for (int j = 0; j <= 100; j++) {
 			System.out.print("j: " + j + "\t");
+			Thread.sleep(1000);
 		}
-		System.out.print("After loops ");
+		System.out.println("After loops ");
 	}
 
 	@Override
 	public void run() {
 		for (int i = 0; i <= 100; i++) {
 			System.out.print("i: " + i + "\t");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				System.out.println("Child thread exiting");
+			}
 		}
 	}
-
 }
