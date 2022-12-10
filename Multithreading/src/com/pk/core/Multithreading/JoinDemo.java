e4a2d2cda76c4a72775568d29a879ec1e6a8d3d2
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-12-10 23:27:02 +0530

    Multi Threaded demo with Join

---
+++ Multithreading/src/com/pk/core/Multithreading/JoinDemo.java
@@ -0,0 +1,35 @@
+package com.pk.core.Multithreading;
+
+import java.util.Scanner;
+
+public class JoinDemo extends Thread {
+	static int n, sum = 0;
+
+	public static void main(String[] args) {
+		System.out.println("sum of first 'N' numbers");
+		System.out.println("enter the value");
+		Scanner In = new Scanner(System.in);
+		JoinDemo.n = In.nextInt();
+
+		JoinDemo jd = new JoinDemo();
+		jd.start();
+		try {
+			jd.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Sum of first " + JoinDemo.n + " Number is " + JoinDemo.sum);
+	}
+
+	@Override
+	public void run() {
+		for (int i = 1; i <= JoinDemo.n; i++) {
+			JoinDemo.sum += i;
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
