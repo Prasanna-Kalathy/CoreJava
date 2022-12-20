bd96b7c8848b627cce1f0aab31f194a61630ee2b
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-12-20 22:49:52 +0530

    Synchronized block with current object

---
+++ Multithreading/src/com/pk/core/Multithreading/sync/blocks/Display.java
@@ -0,0 +1,17 @@
+package com.pk.core.Multithreading.sync.blocks;
+
+public class Display { // Display Class
+	public void SayHello(String name) {// marked synchronized method passes user name
+		System.out.println("Before synchronized block");
+		synchronized (this) {
+			for (int i = 1; i <= 10; i++) {
+				System.out.println("Hello " + name); // greets user
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
