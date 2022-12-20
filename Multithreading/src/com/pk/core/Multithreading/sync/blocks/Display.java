bb2ba337ee8e4c37c8aae780bbf2c44f01bc4be0
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-12-20 22:51:19 +0530

    Synchronized block with Class level object

--- Multithreading/src/com/pk/core/Multithreading/sync/blocks/Display.java
+++ Multithreading/src/com/pk/core/Multithreading/sync/blocks/Display.java
@@ -3,7 +3,7 @@ package com.pk.core.Multithreading.sync.blocks;
 public class Display { // Display Class
 	public void SayHello(String name) {// marked synchronized method passes user name
 		System.out.println("Before synchronized block");
-		synchronized (this) {
+		synchronized (Display.class) {
 			for (int i = 1; i <= 10; i++) {
 				System.out.println("Hello " + name); // greets user
 				try {
