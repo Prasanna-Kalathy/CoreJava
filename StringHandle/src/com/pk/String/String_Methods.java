f64a69e4242486ba59bbe39ac68f3df1a4b5b4c2
Prasanna-Kalathy <prasannakalathy@gmail.com>
2023-01-12 20:13:52 +0530

    String methods experiment

---
+++ StringHandle/src/com/pk/String/String_Methods.java
@@ -0,0 +1,11 @@
+package com.pk.String;
+
+public class String_Methods {
+
+	public static void main(String[] args) {
+		String s = "Naruto";
+		System.out.println(s.indexOf('h'));
+		System.out.println(s.charAt(s.indexOf('o')));
+	}
+
+}
