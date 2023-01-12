08f0b7fbb9bb088099780890c632023337996b18
Prasanna-Kalathy <prasannakalathy@gmail.com>
2023-01-12 23:44:24 +0530

    String Reversal using string builder

---
+++ StringHandle/src/com/pk/String/StringReversal.java
@@ -0,0 +1,15 @@
+package com.pk.String;
+
+public class StringReversal {
+
+	public String Stringrev(StringBuilder s) {
+		s=s.reverse();
+		return s.toString();
+	}
+	public static void main(String[] args) {
+		StringBuilder s=  new StringBuilder("Naruto");
+		StringReversal sr = new StringReversal();
+		System.out.println(sr.Stringrev(s));
+	}
+
+}
