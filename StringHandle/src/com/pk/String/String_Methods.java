074ddb722e562b277373f61e15c0766347e1d318
Prasanna-Kalathy <prasannakalathy@gmail.com>
2023-01-12 23:31:54 +0530

    String methods experiment more

--- StringHandle/src/com/pk/String/String_Methods.java
+++ StringHandle/src/com/pk/String/String_Methods.java
@@ -5,6 +5,7 @@ public class String_Methods {
 	public static void main(String[] args) {
 		String s = "Naruto";
 		System.out.println(s.indexOf('h'));
+		System.out.println(s.replace('h','p'));
 		System.out.println(s.charAt(s.indexOf('o')));
 	}
 
