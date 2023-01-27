db1c1f4ba9864a528fbd2131f911e6786ea1cce5
Prasanna-Kalathy <prasannakalathy@gmail.com>
2023-01-27 12:56:35 +0530

    Reverse an array

---
+++ Asssignments/src/com/pk/core/assignments/arrays/Reverse_array.java
@@ -0,0 +1,11 @@
+package com.pk.core.assignments.arrays;
+
+public class Reverse_array {
+
+	public static void main(String[] args) {
+		String[] arr = { "Naruto", "Uzumaki", "Hinata", "hyuga" };
+		for (int i = arr.length - 1; i >= 0; i--) {
+			System.out.println(arr[i]);
+		}
+	}
+}
