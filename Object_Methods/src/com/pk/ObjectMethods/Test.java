53082eb4c1ebfbed4e88cd4dcfae9eb1447021d0
Prasanna-Kalathy <prasannakalathy@gmail.com>
2023-01-27 13:43:15 +0530

    To string method

---
+++ Object_Methods/src/com/pk/ObjectMethods/Test.java
@@ -0,0 +1,11 @@
+package com.pk.ObjectMethods;
+
+public class Test {
+	public static void main(String[] args) {
+		ToStringDemo ts = new ToStringDemo();
+		ts.setfName("Naruto");
+		ts.setlName("uzumaaki");
+		System.out.println(ts.toString());
+		System.out.println(ts);
+	}
+}
---
+++ Object_Methods/src/com/pk/ObjectMethods/ToStringDemo.java
@@ -0,0 +1,22 @@
+package com.pk.ObjectMethods;
+
+public class ToStringDemo {
+	String fName;
+	String lName;
+
+	public String getfName() {
+		return fName;
+	}
+
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+
+	public String getlName() {
+		return lName;
+	}
+
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+}
