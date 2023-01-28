68ee14c4ae24451b6fe6583229d61e55195c267a
Prasanna-Kalathy <prasannakalathy@gmail.com>
2023-01-28 23:22:05 +0530

    Created collections and Array lists

---
+++ Collections/src/com/pk/Collections/ArrayLists/ArrayLists.java
@@ -0,0 +1,12 @@
+package com.pk.Collections.ArrayLists;
+
+import java.util.ArrayList;
+
+public class ArrayLists {
+
+	public static void main(String[] args) {
+		ArrayList al = new ArrayList();
+		
+	}
+
+}
