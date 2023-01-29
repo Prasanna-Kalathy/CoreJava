0bcc12b96209840f63ba0edfb142666093a0fd53
Prasanna-Kalathy <prasannakalathy@gmail.com>
2023-01-29 20:48:57 +0530

    changes in Array list methods

--- Collections/src/com/pk/Collections/ArrayLists/ArrayListMethods.java
+++ Collections/src/com/pk/Collections/ArrayLists/ArrayListMethods.java
@@ -7,12 +7,15 @@ public class ArrayListMethods {
 
 	public static void main(String[] args) {
 		List<Integer> al = new ArrayList<>();
-		for (int i = 10; i <= 100; i+=10) {
+		for (int i = 10; i <= 50; i += 10) {
 			al.add(i);
 		}
 		System.out.println(al);
 		System.out.println(al.size());
-		al.add(3, 200);
+		for (int i = 50; i <= 100; i += 20) {
+//			al.add(al.size()-1, i);
+			al.add(2,i);
+		}
 		System.out.println(al);
 		System.out.println(al.size());
 		al.set(3, 300);
