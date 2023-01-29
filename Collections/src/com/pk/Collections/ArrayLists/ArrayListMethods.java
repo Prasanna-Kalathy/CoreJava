23ada287d0ae9833c20e2ba5314e694fae85bad6
Prasanna-Kalathy <prasannakalathy@gmail.com>
2023-01-29 22:05:24 +0530

    Add all and Contains all

--- Collections/src/com/pk/Collections/ArrayLists/ArrayListMethods.java
+++ Collections/src/com/pk/Collections/ArrayLists/ArrayListMethods.java
@@ -14,12 +14,33 @@ public class ArrayListMethods {
 		System.out.println(al.size());
 		for (int i = 50; i <= 100; i += 20) {
 //			al.add(al.size()-1, i);
-			al.add(2,i);
+			al.add(2, i);
 		}
 		System.out.println(al);
 		System.out.println(al.size());
 		al.set(3, 300);
 		System.out.println(al);
 		System.out.println(al.size());
+
+		List<Integer> al2 = new ArrayList<>();
+		al2.add(111);
+		al2.add(222);
+		al2.add(333);
+		System.out.println(al2);
+
+		al2.addAll(2, al);
+		System.out.println(al2);
+		System.out.println(al);
+		
+		if(al2.contains(222)) {
+			System.out.println("Found");
+		}else System.out.println("Not Found");
+
+		if(al2.containsAll(al)) {
+			System.out.println("Found");
+		}else System.out.println("Not Found");
+		
+	
+
 	}
 }
