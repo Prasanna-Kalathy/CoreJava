7784b880553e2d949495d7f4993203b09f4f51aa
Prasanna-Kalathy <prasannakalathy@gmail.com>
2023-11-05 19:29:55 +0530

    Reading a file using JAVA

--- IOStream/src/com/IOStreams/Intro/File_Io.java
+++ IOStream/src/com/IOStreams/Intro/File_Io.java
@@ -3,6 +3,7 @@ package com.IOStreams.Intro;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class File_Io {
 	public static void main(String[] args) {
@@ -10,7 +11,13 @@ public class File_Io {
 		try {
 			fis = new FileInputStream(new File("E:\\EDU HUB\\IT Career\\Codes\\CoreJava\\Other_Supporing_Files\\Pk_New.txt"));
 			System.out.println("File Opened");
+			int i;
+			while((i = fis.read())!=-1) {
+				System.out.print((char)i);
+			}
 		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
