8aa90dcb410de34d5e09e6fd4f20838fd7bf1631
Prasanna-Kalathy <prasannakalathy@gmail.com>
2023-11-28 22:51:44 +0530

    Used Buffered Reader to read the line and counted the number of words using string tokenizer

---
+++ IOStream/src/com/IOStreams/Intro/BufferedReaderCount.java
@@ -0,0 +1,33 @@
+package com.IOStreams.Intro;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class BufferedReaderCount {
+
+	public static void main(String[] args) {
+		try {
+			FileReader Fr = new FileReader(
+					new File("E:\\EDU HUB\\IT Career\\Codes\\CoreJava\\Other_Supporing_Files\\Pk_New.txt"));
+			BufferedReader Br = new BufferedReader(Fr);
+			String line;
+			int count = 0;
+			while ((line = Br.readLine()) != null) {
+				StringTokenizer st = new StringTokenizer(line);
+				while (st.hasMoreTokens()) {
+					System.out.println(st.nextToken());
+					count++;
+				}
+			}
+			System.out.println("Total number of words in the file is "+count);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
--- IOStream/src/com/IOStreams/Intro/Copy_File.java
+++ IOStream/src/com/IOStreams/Intro/Copy_File.java
@@ -19,9 +19,17 @@ public class Copy_File {
 					new File("E:\\EDU HUB\\IT Career\\Codes\\CoreJava\\Other_Supporing_Files\\test.jpg"));
 			fos = new FileOutputStream(
 					new File("E:\\EDU HUB\\IT Career\\Codes\\CoreJava\\Other_Supporing_Files\\test_Copy.jpg"));
+			long fileSize = fis.available();
+			long bytesRead = 0;
+
 			int i;
 			while ((i = fis.read()) != -1) {
 				fos.write(i);
+				bytesRead++;
+
+				// Calculate and print the progress as a percentage
+				double progress = ((double) bytesRead / fileSize) * 100;
+				System.out.printf("\r Progress: %.2f%%", progress);
 			}
 			System.out.println("Files Copied sucessfully");
 		} catch (FileNotFoundException e) {
