1909b3f01f0500142558f84827215238e1379a72
Prasanna-Kalathy <prasannakalathy@gmail.com>
2023-12-10 13:48:57 +0530

    Type - Tree Set w random

--- Collections/src/com/pk/Collections/Set/TypesOfHashSet.java
+++ Collections/src/com/pk/Collections/Set/TypesOfHashSet.java
@@ -2,19 +2,21 @@ package com.pk.Collections.Set;
 
 import java.util.LinkedHashSet;
 import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class TypesOfHashSet {
 
 	public static void main(String[] args) {
 		Random rnd = new Random();
 		
-		LinkedHashSet<Integer> Hset = new LinkedHashSet<>();
+		Set<Integer> Tset = new TreeSet<Integer>();
 		
 		for (int i = 0; i <= 5; i++) {
 			int num = rnd.nextInt(10);
-			Hset.add(num);	
+			Tset.add(num);	
 			System.out.println(num);
 		}
-		System.out.println("Hash Set: "+Hset);
+		System.out.println("Tree Set: "+Tset);
 	}
 }
