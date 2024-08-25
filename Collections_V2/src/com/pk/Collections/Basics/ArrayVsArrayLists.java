8c449611956df2bdd13694c3dbea38f47c8e5e01
Prasanna_Kalathy <Prasannakalathy@gmail.com>
2024-08-25 21:00:03 +0530

    Array vs Array List

--- Collections_V2/src/com/pk/Collections/Basics/ArrayVsArrayLists.java
+++ Collections_V2/src/com/pk/Collections/Basics/ArrayVsArrayLists.java
@@ -1,14 +1,15 @@
 package com.pk.Collections.Basics;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ArrayVsArrayLists {
 	public static void main(String[] args) {
-		// Array
+		// Array prints Objects
 		int[] array = { 1, 2, 3, 4, 5 };
 		System.out.println(array);
-		// Array Lists
+		// Array Lists print values
 		List<Integer> ArrLst = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
 		System.out.println(ArrLst);
 	}
